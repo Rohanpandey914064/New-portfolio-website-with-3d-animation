5e8570b8341e3beeb3dd2fff6d963f8480a3cc55
Rohanpandey914064 <rohanrock4019@gmail.com>
2026-03-05 13:18:16 +0530

    add Testimonials section and modify the App section

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import LogoSection from './components/LogoSection'
 import FeatureCards from './sections/FeatureCards'
 import ExperienceSection from './sections/ExperienceSection'
 import TechStack from './sections/TechStack'
+import Testimonials from './sections/Testimonials'
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Hero />
-      <ShowcaseSection />
-      <LogoSection />
-      <FeatureCards />
-      <ExperienceSection />
-      <TechStack />
+      {/* <Hero /> */}
+      {/* <ShowcaseSection /> */}
+      {/* <LogoSection /> */}
+      {/* <FeatureCards /> */}
+      {/* <ExperienceSection /> */}
+      {/* <TechStack /> */}
+      <Testimonials />
     </>
   )
 }
