04e2858e7446c2a73482db0a3ce3d8a13bf06860
Rohan Pandey <rohanrock4019@gmail.com>
2026-04-24 17:59:10 +0530

    Update resume link in Footer component

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className='footer'>
         <div className='footer-container'>
             <div className='flex flex-col justify-center md:items-start items-center'>
-                <a href="https://drive.google.com/file/d/1dlukuwaxxvd9wQVKV-bZ0--oHGhea8ph/view?usp=sharing">Resume</a>
+                <a href="https://drive.google.com/file/d/1HTGhXDwQUv-xvWFPrqRfNQC7B_i4af-_/view?usp=sharing">Resume</a>
             </div>
             <div className='socials'>
                 {socialImgs.map((img) => (
